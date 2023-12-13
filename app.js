b1d630c11fe6527b7a8a1dae5a96a4829e140aa8
Daniela Dos Santos Alves <102709843+Daniela-Alves2004@users.noreply.github.com>
2023-12-13 13:51:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroMax = 5;
+let numeroSecreto = parseInt(Math.random() * numeroMax + 1);
+let chute
+let tentativas = 1
+const btn = document.querySelector('#send')
+
+btn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const name = document.querySelector("#name");
+    name.addEventListener("change", function () {
+        chute = name.value;
+    });
+
+
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+        tentativas++;
+    }
+
+
+    
+    
+})
+
+
+
+
